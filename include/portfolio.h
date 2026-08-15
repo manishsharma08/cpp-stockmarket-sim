968861ddc8abea04f1da417156f61c5131f058da
manishsharma08 <manishsharma282008@gmail.com>
2026-08-15 23:20:49 +0300

    Add portfolio class declaration

---
+++ include/portfolio.h
@@ -0,0 +1,19 @@
+#include <string>
+#pragma once
+#include <unordered_map>
+#include "../include/market.h"
+
+class Portfolio // class that tracks your cash and holdings and does the actual buying and selling
+{
+public:
+    bool buyShare(std::string symbol, int quantity, Market &market);
+    // using market as paramater so we can look up the price from the stock and confirm purchase
+    bool sellShare(std::string symbol, int quantity, Market &market);
+    // same as buy share but it sells share
+    void viewPortfolio(Market &market);
+    // prints cash and every stock held, using market to look up each stock's current price and calculate value
+    Portfolio(double startingCash); // constructor - sets up a new portfolio with a starting amount of cash
+private:
+    double cash;                                   // value of stock
+    std::unordered_map<std::string, int> holdings; // dictionary so show number of shares
+};
