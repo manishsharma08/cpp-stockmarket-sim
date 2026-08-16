431c0b7c54e35ae105e8b2616ac86071ba4b4599
manishsharma08 <manishsharma282008@gmail.com>
2026-08-16 15:04:03 +0300

    Implement Portfolio class with buy/sell/view, tested end-to-end

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 #include "../include/stock.h"
+#include "../include/portfolio.h"
+#include "../include/market.h"
 using namespace std;
 
 int main()
 {
-    Stock apple("AAPL", 190.0);
+    /*Stock apple("AAPL", 190.0);
     cout << apple.getsymbol() << endl;
     cout << apple.getvalue() << endl;
     apple.setprice(195.50);
     cout << apple.getvalue();
-    return 0;
+    return 0;*/
+
+    Market market;
+    market.add("TSLA", 184);
+    market.add("SMSUNG", 177.9);
+    market.add("NVDIA", 196);
+    /*market.listStocks();
+    Stock &t = market.getStock("TSLA");
+    t.setprice(999);
+    cout << t.getsymbol() << " " << t.getvalue();*/
+
+    Portfolio portfolio(10000);
+    portfolio.buyShare("TSLA",2,market);
+    portfolio.viewPortfolio(market);
+    portfolio.sellShare("TSLA",1,market);
+    portfolio.viewPortfolio(market);
+
+
 }
